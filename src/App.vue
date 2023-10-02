04b6618236a10228f8c93711474b60304f76f501
Kacper Tonia <kacpert314@gmail.com>
2023-10-02 16:51:48 +0200

    Up

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>Hello {{ value }}</h1>
-    <p><a href="/.auth/login/auth0">Login</a></p>
+    <div>{{ authMessage }}</div>
+    <ul>
+      <li><a href="/.auth/login/google">Login with Google</a></li>
+      <li><a href="/.auth/login/facebook">Login with Facebook</a></li>
+    </ul>
   </div>
 </template>
 
@@ -11,14 +15,26 @@ export default {
   data() {
     return {
       value: "World",
+      authMessage: "n/a",
     };
+  },
+  mounted() {
+    fetch("/.auth/me")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.clientPrincipal) {
+          this.authMessage = `Hello ${data.clientPrincipal.userDetails}`;
+        }
+      })
+      .catch((e) => {
+        this.authMessage = `Error: ${e}`;
+      });
   },
 };
 </script>
 
 <style scoped>
-h1,
-p {
+h1 {
   text-align: center;
 }
 </style>
